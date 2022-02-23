e8f72f3d0cc10d96ac74763f82c5dc2bb89cc5b6
Wessl <lovewessman@gmail.com>
2022-02-23 09:01:54 +0100

    First edition parsing
    (not functional at all)

---
+++ Assets/Parsing/Parse.py
@@ -0,0 +1,69 @@
+from typing import Literal
+import pyparsing as pp
+from pp import (
+    Literal,
+    Word,
+    Group,
+    Forward,
+    alphas,
+    alphanums,
+    Regex,
+    ParseException,
+    CaselessKeyword,
+    Suppress,
+    delimitedList,
+)
+import math
+import operator
+
+comment = "#" (char) *
+char = ?any ASCII character?
+letter = ?alphabetic characters a−z and A−Z?
+
+identifier = letter (letter | digit | "_") *
+digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+
+# Keywords
+keywords = {
+    k: CaselessKeyword(k)
+    for k in """\
+    child descendant parent root self neighbor label type rowIdx colIdx rowLabel 
+    colLabel last rowLast colLast groupRows groupCols groupRegions if randomSelect eval
+    """.split()
+}
+
+# literals
+float = ["−" | "+"] digit+ "." digit+
+integer = ["−" | "+"] digit+
+number = float | integer
+
+Boolean = "0" | "1"
+
+string = "\"" (char) * "\""
+
+def EBNF():
+
+    # Operators
+    "+" "−" " * " "/" "in" "contains" "!" "&&" "||"
+    ">" "<" "==" ">=" "<=" "!="
+    plus, minus, mult, div = map(Literal, "+-*/")
+    in = Literal("in")
+    contains = Literal("contains")
+    # etc
+
+# Delimiters
+"{" "}" "[" "]" "(" ")" "<" ">"
+"=" "," ":" ";" "−>"
+
+# Whitespace
+whitespace = "\n" | " " | "\t"
+
+# list
+list = "(" expression ("," expression)* ")"
+
+# selection expressions
+selectionExpression = "<" [selectorSeq ("/" selectorSeq) * ] ">"
+selectorSeq = [topoSelector] [attrSelector | groupSelector] *
+topoSelector = funcCall
+attrSelector = "[" boolExpr "]"
+groupSelector = "[" "::" funcCall "]"
