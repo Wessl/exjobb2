1e0066d3288e5b6237d327ea59b4b3f8295a96f1
Love Wessman <lwessman@kth.se>
2022-02-25 17:47:20 +0100

    Parser compiles and has every necessary defintion for grammar, but is probably not functional yet
    There is definitely something missing, cuz this stuff never works on the first try

--- Assets/Parsing/Parse.py
+++ Assets/Parsing/Parse.py
@@ -10,7 +10,65 @@ plusorminus = pp.Literal('+') | pp.Literal('-')
 integer = pp.Combine(pp.Optional(plusorminus) + number)
 digit = pp.Char(pp.nums)
 boolean = pp.Literal("0") | pp.Literal('1')
-#string = pp.QuotedString(pp.alphanums)
+string = pp.QuotedString(quoteChar="'")
+
+# operators
+operators = ('+','-','*','/','in','contains',\
+    '!','&&','||','>','<','==','>=','<=','!')
+plus, minus, mult, div, in_, contains_, not_, and_, or_, gt, lt, eq,\
+    ge, le, ne = map(pp.Literal, operators)
+
+delimiters = pp.oneOf("{ } [ ] ( ) < > = , : ; −>")
+whitespace = pp.oneOf(["\n", " " "\t"])
+
+
+# expression
+expression = pp.Forward()
+funcCall = pp.Forward()
+list_ = pp.Forward()
+
+arithExpr = pp.Forward()
+multiplyExpr = pp.Forward()
+arithOperand = expression | pp.Group(pp.Literal("(") + arithExpr + pp.Literal(")"))
+arithExpr << multiplyExpr + pp.Optional( pp.Group(plus | minus) + multiplyExpr)
+multiplyExpr << arithOperand + pp.Optional( pp.Group(mult | div) + arithOperand)
+
+cmpOp = eq | ne | ge | le | gt | lt
+cmpExpr = arithExpr + pp.Optional(cmpOp + arithExpr)
+
+notExpr = (not_ + boolean) | boolean
+andExpr = notExpr + pp.Optional(and_ + notExpr)
+boolExpr = andExpr + pp.Optional(and_ + andExpr)
+setExpr = pp.Group(expression + in_ + list_) | pp.Group(list_ + contains_ + expression)
+boolOperand = setExpr | cmpExpr | pp.Group("(" + setExpr + ")")
+expression <<= identifier | funcCall | boolExpr
+
+list_ << pp.Literal("(") + expression + pp.OneOrMore(pp.Literal(",") + expression) + ")"
+
+# selection expressions
+
+topoSelector = funcCall
+attrSelector = pp.Literal("[") + boolExpr + pp.Literal("]")
+groupSelector = pp.Literal("[") + pp.Literal("::") + funcCall + pp.Literal("]")
+selectorSeq = pp.Optional(topoSelector) + pp.Optional(attrSelector | groupSelector)
+
+# variable and function
+assignment = identifier + eq + expression
+argList = pp.Optional(expression + pp.Group(pp.ZeroOrMore(pp.Literal(",") + expression)))
+funcCall << identifier + pp.Literal("(") + argList + pp.Literal(")")
+
+selectionExpression = pp.Literal("<") + pp.Optional(selectorSeq)
+
+# 
+# EXECUTION MODEL
+#
+
+actions = pp.OneOrMore(funcCall + pp.Literal(";"))
+rule = pp.Literal("{") + selectionExpression + pp.Literal("->") + actions + pp.Literal("}")
+exit_ = pp.Literal("exit;")
+command = rule | assignment | exit_
+program = pp.ZeroOrMore(command)
+
 
 
 # Keywords
@@ -29,4 +87,6 @@ print(comment.parseString("# this is a comment"))
 print(letter.parseString("hey"))
 print(identifier.parseString("a123_john"))
 print(integer.parseString("-99"))
-print(string.parseString("heyo this is a string, I think. 123 123"))
+print(string.parseString("'heyo this is a string, I think. 123 123'"))
+print(plus.parseString('+'))
+print(delimiters.parseString("};.,"))
