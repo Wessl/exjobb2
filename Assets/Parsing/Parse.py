0974d309b64d816d2c493a591f8702ecaabd0ba7
Wessl <lovewessman@gmail.com>
2022-03-03 21:46:48 +0100

    𝓈𝒽𝒾𝓉 𝒾𝓈 𝒻𝓊𝒸𝓀𝑒𝒹 𝓎𝑜
    no but seriously everything is a little bit better now
    but something is obviously still fucked up who knows idk mang

--- Assets/Parsing/Parse.py
+++ Assets/Parsing/Parse.py
@@ -15,16 +15,16 @@ keywords2 = (child | descendant | parent | root | self_ | neightbour | label | \
 type_ | rowIdx | colIdx | rowLabel | colLabel | last | rowLast |   \
 colLast | groupRows | groupCols | groupRegions | if_ | randomSelect | eval_)
 
-comment = pp.Regex(r"#.*")#.suppress() # don't include comments
+comment = pp.Regex(r"#.*").setName("Comment")#.suppress() # don't include comments
 float_ = pp.pyparsing_common.real
-number = (pp.pyparsing_common.number ^ float_).setName("Number")
-identifier = pp.Word(pp.alphas+"_"+"'", pp.alphanums+"_"+"'").setName("Identifier")
+number = pp.Combine(pp.Opt(pp.oneOf("−-")) + (pp.pyparsing_common.number ^ float_)).setName("Number") # - is different to − ...
+identifier = pp.Word(pp.alphas+"_", pp.alphanums+"_").setName("Identifier")
 
 plusorminus = pp.Literal('+') ^ pp.Literal('-')
 integer = pp.Combine(pp.Optional(plusorminus) + number)
 digit = pp.Char(pp.nums)
 boolean = pp.Literal("0") ^ pp.Literal('1')
-string = pp.QuotedString(quoteChar="\"")
+string = pp.QuotedString(quoteChar="\"").setName("String")
 lparen = pp.Literal("(").suppress().setName("left parenthesis")
 rparen = pp.Literal(")").suppress().setName("right parenthesis")
 lbracket = pp.Literal("[").setName("left bracket")
@@ -46,12 +46,12 @@ contains_ = pp.Keyword("contains")
 expression = pp.Forward()
 
 # function and argument list
-argList = pp.Opt(pp.delimitedList(expression))
+argList = pp.Opt(pp.delimitedList(expression)).setName("ArgList")
 
 funcCall = (identifier + pp.Group(lparen + argList + rparen))
 
 arithExpr = pp.Forward().setName("Arith Expression")
-arithOperand = pp.Group(lparen + arithExpr + rparen) | identifier | number
+arithOperand = pp.Group(lparen + arithExpr + rparen) ^ funcCall ^ identifier ^ number ^ string
 multiplyExpr = arithOperand + pp.Optional((mult ^ div) + arithOperand)
 arithExpr <<= (multiplyExpr + pp.Optional((plus ^ minus) + multiplyExpr))
 list_ = pp.Literal("(") + expression + pp.ZeroOrMore(pp.Literal(",").suppress() + expression) + ")"
@@ -110,37 +110,48 @@ def enableDebug(enabled, testEnabled):
         expression.setName("Expression").setDebug()
         setExpr.setName("Set Expression").setDebug()
         assignment.setName("Assignment").setDebug()
+        selectionExpression.setName("Selection Expression").setDebug()
         lparen.setDebug()
         rparen.setDebug()
-        topoSelector.setDebug()
-        attrSelector.setDebug()
-        groupSelector.setDebug()
-        selectorSeq.setDebug()
+        topoSelector.setName("Topology Selector").setDebug()
+        attrSelector.setName("Attribute Selector").setDebug()
+        groupSelector.setName("Group Selector").setDebug()
+        selectorSeq.setName("Selector Sequence").setDebug()
+        string.setDebug()
+        rule.setName("Rule")
+        actions.setName("Actions")
     if (testEnabled):
         # assignment
         print(assignment.parseString("facW = 17.6;"))
         #C4: add a door touching the ground;
-        print(command.parseString("{ <descendant() [ label == 'facade' ] / [ label == 'main' ] / \
-        cell() [ colLabel == 'mid_col' ][ rowLabel == 'gnd_floor' ] > \
-        −> addShape('door', toGlobalX(0.5), toGlobalY(0.45), 1.76, 2.64, \
-        constrain(dist2region( dist2bottom(0.0, 0.0) ))); }"))
+        print(command.parseString("""{ <descendant() [ label == "facade" ] / [ label == "main" ] / \
+        cell() [ colLabel == "mid_col" ][ rowLabel == "gnd_floor" ] > \
+        −> addShape("door", toGlobalX(0.5), toGlobalY(0.45), 1.76, 2.64, \
+        constrain(dist2region( dist2bottom(0.0, 0.0) ))); }"""))
 
         # function call with list
-        print(funcCall.parseString("rows( lineElem((3.5, 3.0, 4.0), (1, 1), label('gnd_floor')))"))
+        print(funcCall.parseString("""rows( lineElem((3.5, 3.0, 4.0), (1, 1), label("gnd_floor")))"""))
 
         # nested functions & lists
-        print(command.parseString("{ <descendant() [ label == 'facade' ] > −>   \
-        createGrid('main',                                                   \
-        rows( lineElem((3.5, 3.0, 4.0), (1, 1), label('gnd_floor')),         \
-        lineElem((3.0, 3.0, 3.5), (1, 10), label('top_floor'))               \
+        print(command.parseString("""{ <descendant() [ label == "facade" ] > −>   \
+        createGrid("main",                                                   \
+        rows( lineElem((3.5, 3.0, 4.0), (1, 1), label("gnd_floor")),         \
+        lineElem((3.0, 3.0, 3.5), (1, 10), label("top_floor"))               \
         ),                                                                   \
-        cols( lineElem((2.8, 2.5, 3.0), (1, 10), label('left_col')),         \
-        lineElem((3.6, 3.0, 4.0), (1, 1), label('mid_col')),                 \
-        lineElem((2.8, 2.5, 3.0), (2, 2), label('right_col'))                \
-)); }"))
+        cols( lineElem((2.8, 2.5, 3.0), (1, 10), label("left_col")),         \
+        lineElem((3.6, 3.0, 4.0), (1, 1), label("mid_col")),                 \
+        lineElem((2.8, 2.5, 3.0), (2, 2), label("right_col"))               \
+        )); }"""))       
+        # Idk
+        print(command.parseString(""" { <descendant() [ label == "facade" ] / [ label == "main" ] /   \
+        cell() [ colLabel == "left_col" ][ rowIdx == rowLast(−1) ] >                                 \
+        −> addShape("win3", toGlobalX(0.5), toGlobalY(0.5), 1.44, 1.28); }"""))
+        # Idk 2
+        print(command.parseString(""" { <descendant() [ label == "facade" ] / [ label == "main" ] /
+        cell() [ colLabel == "mid_col" ][ rowLabel == "gnd_floor" ] > −>
+        addShape("door", toGlobalX(0.5), toGlobalY(0.45), 1.76, 2.64,
+        constrain(dist2region( dist2bottom(0.0, 0.0) ))); } """))
 enableDebug(True,False)
 
-print(command.parseString("{ <descendant() [ label == 'facade' ] / [ label == 'main' ] / \
- cell() [ colLabel in ('left_col', 'right_col') ][ rowIdx == 1 ] >                          \
- −> addShape('win1', toGlobalX(0.5), toGlobalY(0.5), 1.44, 2.00); }", parseAll=True))
-#print(program.parse_file("selex_file.txt", parseAll=True))
+
+print(program.parse_file("selex_file.txt", parseAll=True))
