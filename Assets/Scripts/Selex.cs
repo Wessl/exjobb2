0250966db2fd325694a027578f9ea9a1825a41d9
Love Wessman <lwessman@kth.se>
2022-03-18 12:39:18 +0100

    Continue work on Selex cell, on attrS and groupS

--- Assets/Scripts/Selex.cs
+++ Assets/Scripts/Selex.cs
@@ -9,6 +9,9 @@ public class Selex : MonoBehaviour
     [SerializeField] private TMP_Dropdown dropdown;
 
     [SerializeField] private Button addTopoSButton;
+
+    [SerializeField] private Button addGroupSButton;
+    [SerializeField] private Button addAttrSButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,8 +31,23 @@ public class Selex : MonoBehaviour
         newDropdown.GetComponent<RectTransform>().localScale = Vector3.one;
         newDropdown.gameObject.SetActive(true);
         // Move down addTopoSButton parent to allow for more buttons...
-        var addTopoSHeight = addTopoSButton.GetComponent<RectTransform>().sizeDelta.y;
-        Debug.Log(addTopoSHeight);
-        addTopoSButton.transform.parent.gameObject.GetComponent<RectTransform>().anchoredPosition += new Vector2(0, -addTopoSHeight);
+        Destroy(addTopoSButton);
+    }
+
+    public void AddGroupSButton()
+    {
+        // Move down both buttons
+        var height = addGroupSButton.GetComponent<RectTransform>().sizeDelta.y;
+        addGroupSButton.transform.parent.gameObject.GetComponent<RectTransform>().anchoredPosition += new Vector2(0, -height);
+        // You should get choice of two buttons, one to pick label attribute and one to pick list attribute thing
+    }
+
+    public void AddAttrSButton()
+    {
+        // Move down both buttons
+        var height = addAttrSButton.GetComponent<RectTransform>().sizeDelta.y;
+        addAttrSButton.transform.parent.gameObject.GetComponent<RectTransform>().anchoredPosition += new Vector2(0, -height);
+        // You should get no choice, just a dropdown of group selector options
+        Instantiate()
     }
 }
