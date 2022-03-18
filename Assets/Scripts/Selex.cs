63a31ed38448fe89cc6ff0ea752b5f7751d38477
Love Wessman <lwessman@kth.se>
2022-03-18 12:12:36 +0100

    Start work on SelexCell layout and functionality
    Pretty good progress

--- Assets/Scripts/Selex.cs
+++ Assets/Scripts/Selex.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Selex : MonoBehaviour
 {
+    [SerializeField] private TMP_Dropdown dropdown;
+
+    [SerializeField] private Button addTopoSButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,17 @@ public class Selex : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    public void AddTopoSButton()
+    {
+        var newDropdown = Instantiate(dropdown, addTopoSButton.transform.position, Quaternion.identity);
+        newDropdown.transform.SetParent(this.transform, true);
+        newDropdown.GetComponent<RectTransform>().localScale = Vector3.one;
+        newDropdown.gameObject.SetActive(true);
+        // Move down addTopoSButton parent to allow for more buttons...
+        var addTopoSHeight = addTopoSButton.GetComponent<RectTransform>().sizeDelta.y;
+        Debug.Log(addTopoSHeight);
+        addTopoSButton.transform.parent.gameObject.GetComponent<RectTransform>().anchoredPosition += new Vector2(0, -addTopoSHeight);
     }
 }
