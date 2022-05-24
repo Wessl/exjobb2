43df70b15fef176f0670bdb238a26d88bffe72f2
Love Wessman <lwessman@kth.se>
2022-05-25 00:19:38 +0900

    Start working on the damage and age component

---
+++ Assets/DamageAndAge.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DamageAndAge : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown damageAgeType;
+    [SerializeField] private TMP_Dropdown noiseModifier;
+    [SerializeField] private TMP_InputField densityPercent;
+    [SerializeField] private TMP_InputField vectorModifier;
+    [SerializeField] private TMP_InputField helperLabel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
