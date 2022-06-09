104ebd34ea571a58c2460e122cd3a93b6e5bc662
Love Wessman <lwessman@kth.se>
2022-06-09 16:43:17 +0900

    Value noise generator functional
    epic, that's enough noise

--- Assets/Scripts/Functions/NoiseGenerator.cs
+++ Assets/Scripts/Functions/NoiseGenerator.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public static class NoiseGenerator
 {
@@ -44,17 +46,20 @@ public static class NoiseGenerator
 
     public static Texture2D ApplyNoiseToTexture(Texture2D sourceTex)
     {
-        
+        int octaves = 4;
         int pixWidth = sourceTex.width;
         int pixHeight = sourceTex.height;
         float stepSize = 1f / pixHeight;
-        sourceTex = new Texture2D(pixWidth, pixHeight);
-        float frequency = 32f;
+        float frequency = 16f;
         Vector3 point00 = new Vector3(-0.5f,-0.5f);
         Vector3 point10 = new Vector3( 0.5f,-0.5f);
         Vector3 point01 = new Vector3(-0.5f, 0.5f);
         Vector3 point11 = new Vector3( 0.5f, 0.5f);
         Random.seed = 42;
+        Texture2D noiseTex = new Texture2D(pixWidth, pixHeight);
+        Color[] pix = new Color[noiseTex.width * noiseTex.height];
+        
+        var sourcePixels = sourceTex.GetPixels();
         for (int y = 0; y < pixHeight; y++)
         {
             Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
@@ -62,31 +67,66 @@ public static class NoiseGenerator
             for (int x = 0; x < pixWidth; x++)
             {
                 Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
-                
-                sourceTex.SetPixel(x, y, Color.white * Value2D(point, frequency));
+                float sample = Sum(point, frequency, octaves) * sourcePixels[(int)y * noiseTex.width + (int)x].a;
+                pix[(int)y * noiseTex.width + (int)x] = new Color(sample, sample, sample);
             }
         }
-
-        sourceTex.Apply();
-        return sourceTex;
+        noiseTex.SetPixels(pix);
+        noiseTex.Apply();
+        return noiseTex;
     }
 
     public static float Value1D(Vector3 point, float frequency)
     {
         point *= frequency;
-        int i = Mathf.FloorToInt(point.x);
-        i &= 15;
-        return hash[i] * (1f / hashMask);
+        int i0 = Mathf.FloorToInt(point.x);
+        i0 &= hashMask;
+        int i1 = i0 + 1;
+        float t = point.x - i0;
+        float h0 = hash[i0];
+        float h1 = hash[i1];
+        t = Smooth(t);
+        return Mathf.Lerp(h0, h1, t) * (1f / hashMask);
     }
     
     public static float Value2D(Vector3 point, float frequency)
     {
         point *= frequency;
+        int ix0 = Mathf.FloorToInt(point.x);
+        int iy0 = Mathf.FloorToInt(point.y);
+        float tx = point.x - ix0;
+        float ty = point.y - iy0;
+        ix0 &= hashMask;
+        iy0 &= hashMask;
+        int ix1 = ix0 + 1;
+        int iy1 = iy0 + 1;
+
+        int h0 = hash[ix0];
+        int h1 = hash[ix1];
+        int h00 = hash[h0 + iy0];
+        int h10 = hash[h1 + iy0];
+        int h01 = hash[h0 + iy1];
+        int h11 = hash[h1 + iy1];
+
+        tx = Smooth(tx);
+        ty = Smooth(ty);
+        
+        return Mathf.Lerp(
+            Mathf.Lerp(h00, h10, tx),
+            Mathf.Lerp(h01, h11, tx),
+            ty) * (1f / hashMask);
+    }
+    
+    public static float Value3D(Vector3 point, float frequency)
+    {
+        point *= frequency;
         int ix = Mathf.FloorToInt(point.x);
         int iy = Mathf.FloorToInt(point.y);
+        int iz = Mathf.FloorToInt(point.z);
         ix &= hashMask;
         iy &= hashMask;
-        return hash[(ix + hash[iy]) & hashMask] * (1f / hashMask);
+        iz &= hashMask;
+        return hash[hash[hash[ix] + iy] +iz] * (1f / hashMask);
     }
 
     private static int[] hash = {
@@ -105,8 +145,44 @@ public static class NoiseGenerator
         218,246, 97,228,251, 34,242,193,238,210,144, 12,191,179,162,241,
         81, 51,145,235,249, 14,239,107, 49,192,214, 31,181,199,106,157,
         184, 84,204,176,115,121, 50, 45,127,  4,150,254,138,236,205, 93,
+        222,114, 67, 29, 24, 72,243,141,128,195, 78, 66,215, 61,156,180,
+        
+        151,160,137, 91, 90, 15,131, 13,201, 95, 96, 53,194,233,  7,225,
+        140, 36,103, 30, 69,142,  8, 99, 37,240, 21, 10, 23,190,  6,148,
+        247,120,234, 75,  0, 26,197, 62, 94,252,219,203,117, 35, 11, 32,
+        57,177, 33, 88,237,149, 56, 87,174, 20,125,136,171,168, 68,175,
+        74,165, 71,134,139, 48, 27,166, 77,146,158,231, 83,111,229,122,
+        60,211,133,230,220,105, 92, 41, 55, 46,245, 40,244,102,143, 54,
+        65, 25, 63,161,  1,216, 80, 73,209, 76,132,187,208, 89, 18,169,
+        200,196,135,130,116,188,159, 86,164,100,109,198,173,186,  3, 64,
+        52,217,226,250,124,123,  5,202, 38,147,118,126,255, 82, 85,212,
+        207,206, 59,227, 47, 16, 58, 17,182,189, 28, 42,223,183,170,213,
+        119,248,152,  2, 44,154,163, 70,221,153,101,155,167, 43,172,  9,
+        129, 22, 39,253, 19, 98,108,110, 79,113,224,232,178,185,112,104,
+        218,246, 97,228,251, 34,242,193,238,210,144, 12,191,179,162,241,
+        81, 51,145,235,249, 14,239,107, 49,192,214, 31,181,199,106,157,
+        184, 84,204,176,115,121, 50, 45,127,  4,150,254,138,236,205, 93,
         222,114, 67, 29, 24, 72,243,141,128,195, 78, 66,215, 61,156,180
     };
+    
+    private static float Smooth (float t) {
+        return t * t * t * (t * (t * 6f - 15f) + 10f);
+    }
+    
+    public static float Sum (Vector3 point, float frequency, int octaves)
+    {
+        float sum = Value2D(point, frequency);
+        float amplitude = 1f;
+        float range = 1f;
+        for (int o = 1; o < octaves; o++)
+        {
+            frequency *= 2f;
+            amplitude *= 0.5f;
+            range += amplitude;
+            sum += Value2D(point, frequency) * amplitude;
+        }
+        return sum / range;
+    }
 
     private const int hashMask = 255;
 }
