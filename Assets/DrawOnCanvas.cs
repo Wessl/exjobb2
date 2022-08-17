258e7db0cf79630c624f7bf9c19167508ae4fb02
Love Wessman <lwessman@kth.se>
2022-08-18 04:15:00 +0900

    Keep working on DrawOnCanvas

--- Assets/DrawOnCanvas.cs
+++ Assets/DrawOnCanvas.cs
@@ -9,20 +9,21 @@ public class DrawOnCanvas : MonoBehaviour
 {
     GraphicRaycaster raycaster;
     public Image lineImage;
-
+    private bool clickIsDown;
+    private Vector2 downPos;
 
     private void Awake()
     {
         this.raycaster = GetComponent<GraphicRaycaster>();
-        
+        clickIsDown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Check if the left Mouse button is clicked
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            this.GetComponentInParent<ScrollRect>().enabled = false;
             //Set up the new Pointer Event
             PointerEventData pointerData = new PointerEventData(EventSystem.current);
             List<RaycastResult> results = new List<RaycastResult>();
@@ -30,8 +31,30 @@ public class DrawOnCanvas : MonoBehaviour
             //Raycast using the Graphics Raycaster and mouse click position
             pointerData.position = Input.mousePosition;
             this.raycaster.Raycast(pointerData, results);
+
+            downPos = pointerData.position;
+            foreach (var result in results)
+            {
+                Debug.Log(result.worldPosition);
+            }
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            
+            this.GetComponentInParent<ScrollRect>().enabled = true;
+
+            //Set up the new Pointer Event
+            PointerEventData pointerData = new PointerEventData(EventSystem.current);
+            List<RaycastResult> results = new List<RaycastResult>();
  
-            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+            //Raycast using the Graphics Raycaster and mouse click position
+            pointerData.position = Input.mousePosition;
+            this.raycaster.Raycast(pointerData, results);
+            var endPos = pointerData.position;
+            var dirVec = (endPos - downPos);
+            var distance = dirVec.magnitude;
             foreach (RaycastResult result in results)
             {
                 Debug.Log("Hit " + result.gameObject.name);
@@ -39,7 +62,14 @@ public class DrawOnCanvas : MonoBehaviour
                 var lineGO = lineImg.gameObject;
                 lineGO.SetActive(true);
                 lineGO.transform.SetParent(gameObject.transform, true);
+                var rect = lineGO.GetComponent<RectTransform>();
+                rect.sizeDelta = new Vector2(distance,10);
+                rect.position -= new Vector3(dirVec.x / 2, dirVec.y / 2, 0);
+                // rect.Rotate(dirVec, );
+                Debug.Log(distance);
+                // lineGO.transform.Rotate();
             }
         }
     }
 }
+
