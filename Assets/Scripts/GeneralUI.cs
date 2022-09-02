c46499825e5691dc02c70e0beac39048e717414e
Love Wessman <lwessman@kth.se>
2022-09-02 20:04:43 +0900

    add revert construction button
    wonky implementation code-wise but seems to work!

--- Assets/Scripts/GeneralUI.cs
+++ Assets/Scripts/GeneralUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class GeneralUI : MonoBehaviour
 {
     [SerializeField] private GameObject scrollablePanel;
+    private GameObject originalRoot;
     public void ToggleUIVisible()
     {
         CanvasGroup cg = scrollablePanel.GetComponentInChildren<CanvasGroup>();
@@ -13,5 +15,21 @@ public class GeneralUI : MonoBehaviour
         GameObject.FindObjectOfType<Finalization>().camCanMove = !cg.interactable;
         Camera.main.transform.rotation = Quaternion.identity;
         Camera.main.transform.position = new Vector3(0, 0, -10);
+    }
+
+    private void Start()
+    {
+        originalRoot = Instantiate(GameObject.FindWithTag("Root"), Vector3.zero, Quaternion.identity);
+        originalRoot.SetActive(false);
+    }
+
+    public void DeleteAllFacade()
+    {
+        // removes every constructed object, reverts back to OG
+        var newRoot = GameObject.FindWithTag("Root");
+        Destroy(newRoot);
+        originalRoot.SetActive(true);
+        originalRoot = Instantiate(originalRoot, Vector3.zero, Quaternion.identity);
+        originalRoot.SetActive(false);
     }
 }
